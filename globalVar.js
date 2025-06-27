2546bfdb1d5c0daf543dd6184d3273c670102ca4
Bill Chu <shui.bill.chu@gmail.com>
2025-06-26 20:53:18 -0700

    Stop tracking globalVar.js

--- globalVar.js
+++
@@ -1,7 +0,0 @@
-// == Configuration ==
-//using google map project in my google colud console
-const MAPS_API_KEY = 'AIzaSyCo8p626MNZc6UPcxDVaChgjEqZ9To75Co';
-const SEARCH_RADIUS = 0.5; //find another address within SEARCH_RADIUS of the actual address
-const IsObfuscated = true; // Set to false to skip obfuscation
-//used on createMapGroup function
-const DEPLOYMENT_ID = 'AKfycby7VyEEX6eVCmxRyCGRypTLsYVl7gbAB28I6u6vWAk';  // Replace with actual ID
